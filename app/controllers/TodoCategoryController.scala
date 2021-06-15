8959d1ad9c974abcfd3dd8107255c86e003e70c5
Arata-Kurokawa <arata.kurokawa@nextbeat.net>
2021-06-15 11:36:28 +0900

    カテゴリ一覧の実装

--- app/controllers/TodoCategoryController.scala
+++ app/controllers/TodoCategoryController.scala
@@ -7,18 +7,28 @@ import play.api.data.Forms._
 import play.api.data.validation.Constraints._
 import play.api.i18n.I18nSupport
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 import model.ViewValueHome
 import model.TodoCategoryModel
 
+import presenters.TodoCategoryListPresenter
+
 class TodoCategoryController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
-  def index() = Action { implicit req =>
+  def index() = Action.async { implicit req =>
     val vv = ViewValueHome(
       title  = "Todoカテゴリ一覧",
       cssSrc = Seq("reset.css", "main.css"),
       jsSrc  = Seq("main.js")
     )
 
-    Ok(views.html.todoCategory.Index(vv))
+    for {
+      categories <- TodoCategoryModel.all
+    } yield {
+      val presenter = new TodoCategoryListPresenter(categories)
+      Ok(views.html.todoCategory.Index(vv, presenter))
+    }
   }
 
   def add() = Action { implicit req =>
@@ -29,5 +39,19 @@ class TodoCategoryController @Inject()(val controllerComponents: ControllerCompo
     )
 
     Ok(views.html.todoCategory.Add(vv))
+  }
+
+  def edit(id: Long) = Action { implicit req =>
+    val vv = ViewValueHome(
+      title  = "Todoカテゴリ編集",
+      cssSrc = Seq("reset.css", "main.css"),
+      jsSrc  = Seq("main.js")
+    )
+
+    Ok(views.html.todoCategory.Edit(vv))
+  }
+
+  def remove(id: Long) = Action { implicit req =>
+    Redirect(routes.TodoCategoryController.index)
   }
 }
