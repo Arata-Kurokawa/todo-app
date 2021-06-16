1c45261d6d29319897701a13b212eb486c0f4c90
Arata-Kurokawa <arata.kurokawa@nextbeat.net>
2021-06-16 10:22:17 +0900

    usecaseのリファクタリング

--- app/useCase/Todo.scala
+++ app/useCase/Todo.scala
@@ -3,36 +3,31 @@ package useCase
 
 import scala.concurrent.Future
 
-import lib.persistence.onMySQL
+import lib.persistence.onMySQL.TodoRepository
 import lib.model.Todo
 import lib.model.Todo._
 import lib.model.TodoCategory
 
 object TodoUseCase {
   def get(id: Id): Future[Option[EmbeddedId]] = {
-    val repository = onMySQL.TodoRepository
-    repository.get(id)
+    TodoRepository.get(id)
   }
 
   def all(): Future[Seq[EmbeddedId]] = {
-    val repository = onMySQL.TodoRepository
-    repository.all()
+    TodoRepository.all()
   }
 
   def create(title: String, body: String, categoryId: Long): Future[Id] = {
-    val repository = onMySQL.TodoRepository
     val entity = Todo(TodoCategory.Id(categoryId), title, body, Todo.Status.WAITING)
-    repository.add(entity)
+    TodoRepository.add(entity)
   }
 
   def update(todo: EmbeddedId, title: String, body: String, state: Short, categoryId: Long): Future[Option[EmbeddedId]] = {
-    val repository = onMySQL.TodoRepository
     val newTodo = todo.v.copy(title = title, body = body, state = Todo.Status(state), categoryId = TodoCategory.Id(categoryId))
-    repository.update(new Todo.EmbeddedId(newTodo))
+    TodoRepository.update(new Todo.EmbeddedId(newTodo))
   }
 
   def remove(id: Id): Future[Option[EmbeddedId]] = {
-    val repository = onMySQL.TodoRepository
-    repository.remove(id)
+    TodoRepository.remove(id)
   }
 }
--- app/useCase/TodoCategory.scala
+++ app/useCase/TodoCategory.scala
@@ -4,31 +4,27 @@ package useCase
 import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 
-import lib.persistence.onMySQL
+import lib.persistence.onMySQL.TodoCategoryRepository
 
 import lib.model.TodoCategory
 import lib.model.TodoCategory._
 
 object TodoCategoryUseCase {
   def get(id: Long): Future[Option[EmbeddedId]] = {
-    val repository = onMySQL.TodoCategoryRepository
-    repository.get(Id(id))
+    TodoCategoryRepository.get(Id(id))
   }
 
   def all(): Future[Seq[EmbeddedId]] = {
-    val repository = onMySQL.TodoCategoryRepository
-    repository.all()
+    TodoCategoryRepository.all()
   }
 
   def create(name: String, slug: String): Future[Id] = {
-    val repository = onMySQL.TodoCategoryRepository
     val entity = TodoCategory(name, slug, Color.RED)
-    repository.add(entity)
+    TodoCategoryRepository.add(entity)
   }
 
   def update(category: EmbeddedId, name: String, slug: String, color: Color): Future[Option[EmbeddedId]] = {
-    val repository = onMySQL.TodoCategoryRepository
     val newCategory = category.v.copy(name = name, slug = slug, color = color)
-    repository.update(new TodoCategory.EmbeddedId(newCategory))
+    TodoCategoryRepository.update(new TodoCategory.EmbeddedId(newCategory))
   }
 }
