2d674072fced565df75c5d79dda7d6f35b19b1a5
Arata-Kurokawa <arata.kurokawa@nextbeat.net>
2021-06-17 14:16:10 +0900

    Todo一覧のカテゴリに色を付けた

--- app/model/TodoList.scala
+++ app/model/TodoList.scala
@@ -9,7 +9,8 @@ case class ViewValueTodoList(items: Seq[Item], home: ViewValueHome) {
 }
 
 object ViewValueTodoList {
-  case class Item(id: Long, title: String, body: String, state: Todo.Status, categoryName: String)
+  case class Category(name: String, color: TodoCategory.Color)
+  case class Item(id: Long, title: String, body: String, state: Todo.Status, category: Option[Category])
 
   def apply(
     todoList: Seq[Todo.EmbeddedId],
@@ -18,8 +19,8 @@ object ViewValueTodoList {
     val items = for {
       todo <- todoList
     } yield {
-      val category = categories.find(_.id == todo.v.categoryId)
-      ViewValueTodoList.Item(todo.id, todo.v.title, todo.v.body, todo.v.state, category.map(_.v.name).getOrElse(""))
+      val category = categories.find(_.id == todo.v.categoryId).map(c => Category(c.v.name, c.v.color))
+      ViewValueTodoList.Item(todo.id, todo.v.title, todo.v.body, todo.v.state, category)
     }
 
     val home = ViewValueHome(
